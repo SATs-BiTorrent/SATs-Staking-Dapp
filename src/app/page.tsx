d3fe4a5aaa808fbb9acecfc7371c1d2240a58817
Kagiso Lesiba Kojo Papo <contact@sats.capetown>
2024-08-28 05:12:40 +0200

    SATs Dapp v1

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { StakeToken } from "../../components/StakeToken";
+import { client } from "./client";
+import { chain } from "./chain";
+import { ConnectEmbed } from "@/app/thirdweb";
+
+
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw",
+    }}>
+      <div style={{ textAlign: "center" }}>
+        <ConnectEmbed
+          client={client}
+          chain={chain}
+        />
+        <StakeToken />
+      </div>
+    </div>
+  );
+}
