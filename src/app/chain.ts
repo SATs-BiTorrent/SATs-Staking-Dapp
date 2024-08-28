59af5928ac3ae2aca4858a0ed7c0a08e6f5559ba
Kagiso Lesiba Kojo Papo <contact@sats.capetown>
2024-08-28 05:08:01 +0200

    Update

--- src/app/chain.ts
+++
@@ -1,4 +0,0 @@
-import { defineChain } from "thirdweb";
-
-// Replace <chain_id> with the chain id of your chain
-export const chain = defineChain( 199 );
--- src/app/client.ts
+++
@@ -1,7 +0,0 @@
-import { createThirdwebClient } from 'thirdweb';
-
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
-
-export const client = createThirdwebClient({
-    clientId: CLIENT_ID,
-});
